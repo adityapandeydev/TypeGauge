2e8015c13c53aabfc7a508485b0dae2aed8e66ad
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-01-01 19:26:52 +0530

    fixed bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import RaceOn from "./components/RaceOn";
 import RaceTrack from "./components/RaceTrack";
 import TypingArea from "./components/TypingArea";
+import Result from "./components/Result";
+import { useTypingStore } from "./stores/useTypingStore";
 
 const App = () => {
+  const raceComplete = useTypingStore((state) => state.raceCompleted);
   return (
     <div className="bg-lightGray grid justify-center items-start pt-20 min-h-screen">
       <div className="w-[800px] p-10 space-y-6 rounded-md bg-white drop-shadow-xl">
@@ -10,7 +13,7 @@ const App = () => {
 
         <RaceTrack />
 
-        <TypingArea />
+        {!raceComplete ? <TypingArea /> : <Result />}
       </div>
     </div>
   );
---
+++ src/components/Result.tsx
@@ -0,0 +1,11 @@
+const Result = () => {
+    return (
+        <div>
+            <button className="bg-greenA hover:bg-greenB px-8 py-2 text-white rounded-lg">
+                New Race
+            </button>
+        </div>
+    )
+}
+
+export default Result;
